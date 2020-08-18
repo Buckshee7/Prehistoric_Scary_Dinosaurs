1cc5acfb10dba9d46369f044f699cd842ae9d0b4
Timothy Saudners <timsaunders88@gmail.com>
2020-08-18 20:03:11 +0100

    added test for park:should have name

---
+++ specs/park_spec.js
@@ -0,0 +1,38 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+  let dino1, dino2, dino3;
+  let park1;
+  beforeEach(function () {
+    dino1 = new Dinosaur("TRex", "carnivore", 10);
+    dino2 = new Dinosaur("Stegasaurus", "herbavore", 5);
+    dino3 = new Dinosaur("Triceratops", "herbavore", 15)
+    park1 = new Park("Prehistoric Scary Dinosaurs 61", 50, [dino1, dino2])
+  });
+
+  it('should have a name', function(){
+    const actual = park1.name;
+    assert.strictEqual(actual, "Prehistoric Scary Dinosaurs 61");
+  });
+
+  it('should have a ticket price');
+
+  it('should have a collection of dinosaurs');
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to calculate the total number of visitors per day');
+
+  it('should be able to calculate the total number of visitors per year');
+
+  it('should be able to calculate total revenue for one year');
+
+});
